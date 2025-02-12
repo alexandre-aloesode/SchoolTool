7c5cf50a51e1239bdcf63e7ea5f2e548d1a3d0ad
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2025-02-12 13:04:12 +0100

    maj

--- front/app/(tabs)/_layout.tsx
+++ front/app/(tabs)/_layout.tsx
@@ -1,60 +1,66 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React, { useContext } from "react";
+import { Platform } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import AuthContext from "@/context/authContext";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
+  const auth = useContext(AuthContext);
+  // console.log("auth", auth.user);
+  // console.log("authtype", typeof(auth.user));
+  
+  
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-            backgroundColor: '#0084FA',
+            position: "absolute",
+            backgroundColor: "#0084FA",
           },
           default: {
             paddingTop: 4,
-            backgroundColor: '#0084FA',
+            backgroundColor: "#0084FA",
           },
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: '',
+          title: "",
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color="white" />,
         }}
       />
       <Tabs.Screen
         name="jobs"
         options={{
-          title: '',
+          title: "",
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="briefcase.fill" color="white" />,
         }}
       />
       <Tabs.Screen
         name="absences"
         options={{
-          title: '',
+          title: "",
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.crop.circle.badge.xmark" color="white" />,
         }}
       />
       <Tabs.Screen
         name="calendar"
         options={{
-          title: '',
+          title: "",
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="calendar.badge.clock" color="white" />,
         }}
       />
