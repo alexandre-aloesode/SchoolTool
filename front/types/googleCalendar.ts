62de8e5e7c5486924b7ca0f86532638752681c19
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2025-07-09 14:33:01 +0200

    fixed typing errors in googleCalendar.tsx

---
+++ front/types/googleCalendar.ts
@@ -0,0 +1,21 @@
+
+export type GoogleCalendarEvent = {
+    id: string;
+    summary: string;
+    start: {
+      dateTime?: string;
+      date?: string;
+    };
+    end?: {
+      dateTime?: string;
+      date?: string;
+    };
+    description?: string;
+    location?: string;
+    htmlLink?: string;
+    attendees?: Array<{
+      email: string;
+      responseStatus: string;
+    }>;
+  };
+  
